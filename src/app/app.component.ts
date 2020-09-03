89459530ee64607651fd5da1e1b1a1b879ebd9e1
Allan <vredes@live.com>
2020-09-02 23:55:58 -0400

    service fixed 50%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PeliculasService } from "./services/peliculas.service"
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peliculas-app';
+  
+  constructor( public ps:PeliculasService ) {
+
+    this.ps.getPopulares()
+      .subscribe( data => console.log(data) )
+
+  }
 
 
 
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,30 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
  providedIn: 'root'
 })
-export class PeliculaService {
-  
-  private apikey: string = '03d4b8b50b89b8a6680192023f6cdde0';
-  private urlMovie: string = 'https://api.themoviedb.org/3';
+export class PeliculasService {
 
   constructor(private http: HttpClient) { }
 
-  getPopulares(){
+  getQueryJsonP(query: string) {
 
-    let url = `${this.urlMovie}discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es`;
+    let apiKey: string='08ba5c8d49c4d5b05eb685b51c17cc2b';
 
-    return this.http.get( url )
-      .pipe(map( (res: any) => {
+    const urlMovieDb = `https://api.themoviedb.org/3/${query}${apiKey}&language=es&callback=JSONP_CALLBACK`;
 
-        console.log(res.results);
-        return res.results;
-
-      }));
+    return this.http.jsonp( urlMovieDb, 'callback' );
   }
 
+  getPopulares(){
+    return this.getQueryJsonP('/discover/movie?sort_by=popularity.desc?&apiKey=').pipe(map( res => res ));
+  }
 }
 
   //To use jsonp, call http method as 'http.jsonp(url, 'callback');
