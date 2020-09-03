15f79d2761ecb73757be6e0e304ea56bc2b72699
Allan <vredes@live.com>
2020-09-03 00:16:36 -0400

    service fixed 100%

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,28 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientJsonpModule } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+
 @Injectable({
- providedIn: 'root'
+  providedIn: 'root'
 })
 export class PeliculasService {
 
-  constructor(private http: HttpClient) { }
+  constructor(  private  http: HttpClient, private jsonp: HttpClientJsonpModule ) { }
 
-  getQueryJsonP(query: string) {
+  getQuery(query: string) {
 
     let apiKey: string='08ba5c8d49c4d5b05eb685b51c17cc2b';
 
-    const urlMovieDb = `https://api.themoviedb.org/3/${query}${apiKey}&language=es&callback=JSONP_CALLBACK`;
+    const urlMovieDb = `https://api.themoviedb.org/3/${query}${apiKey}&language=es`;
 
-    return this.http.jsonp( urlMovieDb, 'callback' );
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjliMjFkOTJmNjJkZDYwZTQxNzU3Yzc0ZDAzZDcyOCIsInN1YiI6IjVlNmJmYmM0MzU3YzAwMDAxMTQwYWVlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZxnGubGV4gRNjR6wB-hwoSHyz75GnkjmIMUi8jsJFlE'
+    });
+
+    return this.http.get(urlMovieDb, {headers});
+
   }
 
-  getPopulares(){
-    return this.getQueryJsonP('/discover/movie?sort_by=popularity.desc?&apiKey=').pipe(map( res => res ));
+  getPopulares() {
+    return this.getQuery('/discover/movie?sort_by=popularity.desc?&apiKey=').pipe(map( res => res ));
   }
+
 }
-
   //To use jsonp, call http method as 'http.jsonp(url, 'callback');
 
 
