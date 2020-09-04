c25129d886e5bc36ed2563bb329981be8fb80755
Allan <vredes@live.com>
2020-09-04 00:00:41 -0400

    navbar and routes working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
+//Servicios
+import { PeliculasService } from "./services/peliculas.service"
+
+import { APP_ROUTING } from "./app.routes"
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PeliculaComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpClientJsonpModule
+    HttpClientJsonpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    PeliculasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
